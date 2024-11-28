7cdf584e7407c5966bedae50c1d817c0b1a0245a
mayssaGH <mayssagnm@gmail.com>
2024-11-28 14:51:07 +0100

    configure google maps api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,12 @@ android {
 }
 
 dependencies {
-    implementation("org.osmdroid:osmdroid-android:6.1.11")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation ("com.google.android.gms:play-services-maps:18.1.0")
+    implementation ("com.google.android.gms:play-services-location:20.0.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
